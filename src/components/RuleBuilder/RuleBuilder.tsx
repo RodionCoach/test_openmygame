76e2eef8dfa1da3228f4bb601fd3441c027a8c02
RodionCoach <25160654+RodionCoach@users.noreply.github.com>
2025-05-27 18:47:06 +0300

    fix: modal view

--- src/components/RuleBuilder/RuleBuilder.tsx
+++ src/components/RuleBuilder/RuleBuilder.tsx
@@ -17,18 +17,15 @@ const RuleBuilder: React.FC = () => {
           title="Reset"
         >
           <svg
+            fill="#000000"
+            height="24px"
+            width="24px"
+            version="1.1"
+            id="Capa_1"
             xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
+            viewBox="0 0 27.971 27.971"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4 4v5h.582M19.418 19A9 9 0 106.582 6.582"
-            />
+            <path d="M23.92,14.746l-4.05-4.051h2.374l-0.068-0.177c-1.407-3.561-4.882-6.088-8.95-6.088c-5.312,0-9.62,4.307-9.62,9.616 c0,5.316,4.308,9.623,9.62,9.623c3.907,0,7.271-2.128,8.775-5.479l3.854,0.039c-0.013,0.03-3.032,8.918-12.693,8.918 C5.893,27.148,0,21.254,0,13.987C0,6.715,5.893,0.824,13.161,0.824c6.08,0,11.195,4.116,12.709,9.715l0.032,0.156h2.069 L23.92,14.746z"></path>
           </svg>
         </Button>
       </div>
--- src/components/common/modal/Modal.tsx
+++ src/components/common/modal/Modal.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { createPortal } from "react-dom";
 
 interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
@@ -8,12 +9,13 @@ interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {
 const Modal: React.FC<ModalProps> = ({ isOpen, children }) => {
   if (!isOpen) return null;
 
-  return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-40 z-1">
+  return createPortal(
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-40">
       <div className="bg-white rounded shadow-lg p-6 min-w-[300px]">
         {children}
       </div>
-    </div>
+    </div>,
+    document.body.querySelector("#root-modal") as HTMLElement
   );
 };
 
